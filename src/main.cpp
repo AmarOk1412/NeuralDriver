fac2191a39b84abc3a5364d74195cef4d59777a8
AmarOk1412 <amarok@enconn.fr>
2015-03-06 10:58:03 +0100

    test

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,7 @@ int main() {
   Neuron n;
   std::vector<double> t;
   
-  t.push_back(0);
+  /*t.push_back(0);
   t.push_back(0);
   std::cout << n.calcOutput(t) << std::endl;
   t.clear();
@@ -23,7 +23,7 @@ int main() {
   t.push_back(1);
   t.push_back(1);
   std::cout << n.calcOutput(t) << std::endl;
-  t.clear();
+  t.clear();*/
 
   return 0;
 }
